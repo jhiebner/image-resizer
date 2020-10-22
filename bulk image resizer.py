aa15fdd0914a73f0363546dc34a2e16e01e5d193
jhiebner <32879595+jhiebner@users.noreply.github.com>
2020-10-22 15:17:50 -0500

    Add files via upload

---
+++ bulk image resizer.py
@@ -0,0 +1,66 @@
+from tkinter import filedialog
+from tkinter import *
+from PIL import Image, ImageFilter
+import os
+
+
+root = Tk()
+root.title("Bulk Image Resizer")
+
+
+ 
+def get_pull_folder():
+    pull_folder = filedialog.askdirectory()
+    p_folder = Label(root, text = f'Pull Folder: {pull_folder}')
+    p_folder.pack()
+    return pull_folder
+
+
+def get_save_folder():
+    save_folder = filedialog.askdirectory()
+    s_folder = Label(root, text = f'Save folder: {save_folder}')
+    s_folder.pack()
+    return save_folder
+
+
+def resize_img():
+    message = Label(root, text = 'Resizing has started')
+    message.pack()
+    for filename in os.listdir(folder_name):
+        if filename.endswith('.png'):
+            img = Image.open(folder_name + "/" + filename)
+            new_img = img.resize((115,115))
+            new_img.save(save_folder + "/" + filename)
+            print(f' {filename} resized!')
+        else: pass
+
+
+folder_name = get_pull_folder()
+save_folder = get_save_folder()
+
+resize_button = Button(
+    text = "Resize folder Images",
+    command = resize_img
+    )
+resize_button.pack()
+
+root.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
